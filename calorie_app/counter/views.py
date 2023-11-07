0bd6dc1bc42b2a60c5ace334d3ab948d743f45e5
Assen95 <asenjo95@gmail.com>
2023-11-07 12:50:42 +0200

    Added API, changed tags, added functionality to form and statements

--- calorie_app/counter/views.py
+++ calorie_app/counter/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import json
 
 
 def home(request):
-
-    query = '1lb brisket and fries'
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'wP0qjZRvN+TgOsDjBQm7BQ==kax9fC3z7V4ZIPia'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query, headers={'X-Api-Key': 'wP0qjZRvN+TgOsDjBQm7BQ==kax9fC3z7V4ZIPia'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = 'there was an error!'
+            print(e)
+        context = {
+            'api': api
+        }
+        return render(request, 'home.html', context)
     else:
-        print("Error:", response.status_code, response.text)
-    return render(request, 'home.html')
+        context = {
+            'query': "Enter a valid query!",
+        }
+        return render(request, 'home.html', context)
